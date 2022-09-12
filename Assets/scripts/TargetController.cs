ca33372d9dc02138397f1f1c1df249cd626b140d
Sam "Shimi" Halperin <sqh@me.com>
2022-09-11 22:05:27 -0400

    Update TargetController.cs
    Use coroutine to LERP position

--- Assets/scripts/TargetController.cs
+++ Assets/scripts/TargetController.cs
@@ -5,40 +5,32 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour
 {
-    private bool doMove = false;
     private GameObject camera;    
-    private int LERP_SPEED = 2;
     private Vector3 newPosition;
-    private float MIN_DISTANCE = .2f;
-    
-    
+    float LERP_TIME = .7f; 
+       
     // Start is called before the first frame update
     void Start()
     {
         camera = GameObject.FindGameObjectWithTag("MainCamera");
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (doMove)
-            if (Vector3.Distance(camera.transform.position, newPosition) > MIN_DISTANCE)
-            {
-                camera.transform.position = Vector3.Lerp(camera.transform.position, newPosition, LERP_SPEED * Time.deltaTime);    
-            }
-            else
-            {
-                doMove = false;
-            }
     }
 
     private void OnMouseDown()
     {
         newPosition = transform.position;
         newPosition.y = camera.transform.position.y;
-        doMove = true;
+         StartCoroutine(MoveToPosition());   
     }
 
- 
+    private IEnumerator MoveToPosition()
+    {
+        var elapsedTime = 0f;
+        var startingPos = camera.transform.position;
+        while (elapsedTime < LERP_TIME)
+        {
+            camera.transform.position = Vector3.Lerp(startingPos, newPosition, (elapsedTime / LERP_TIME));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
