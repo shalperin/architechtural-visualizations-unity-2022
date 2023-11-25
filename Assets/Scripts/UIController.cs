0bd07ca6294aa2bfb094ee6eea8afba146521e08
Sam "Shimi" Halperin <sqh@me.com>
2023-11-24 21:43:42 -0500

    2023 menu update

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -14,108 +14,62 @@ public class UIController : MonoBehaviour
     private const float VISIBLE = 1f;
     private const float INVISIBLE = 0f;
 
-    public GameObject s_witch;
-    public Image x;
     public Image instructions;
     public Image floorplan;
-    
-    private bool floorplanVisible = false;
-    private bool instructionsVisible = false;
+
 
     public bool isInMenuSystem = false;
     
     private void Start()
     {
-        s_witch.SetActive(true);
-        ShowX();
-        HideFloorplan(true);
-        ShowInstructions();
+        Debug.Log("start");
+        floorplan.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(true);
     }
 
     public void CloseAll()
     {
-        Debug.Log("closing all");
-        HideFloorplan();
-        HideInstructions();
-        HideX();
+        floorplan.gameObject.SetActive(false);
+        instructions.gameObject.SetActive(false);
     }
     
     public void Toggle(String which)
     {
-        Debug.Log("toggleing");
-
         if (which == FLOORPLAN)
         {
-            if (floorplanVisible)
-            {
-                HideFloorplan();
-                HideInstructions();
-                HideX();
-            }
-            else
-            {
-                HideInstructions();
-                ShowFloorpan();
-                ShowX();
-            }
-            
-        } else if (which == INSTRUCTIONS)
-        {
-            if (instructionsVisible)
-            {
-                HideFloorplan();
-                HideInstructions();
-                HideX();
-            }
-            else
-            {
-                HideFloorplan();
-                ShowInstructions();
-                ShowX();
-            }
+            instructions.gameObject.SetActive(false);
+            floorplan.gameObject.SetActive(!floorplan.gameObject.activeSelf);
         }
-        
-        
+        else if (which == INSTRUCTIONS)
+        {
+            floorplan.gameObject.SetActive(false);
+            instructions.gameObject.SetActive(!instructions.gameObject.activeSelf);
+        }        
     }
     
-    private void HideX()
-    {
-        x.GetComponent<CanvasRenderer>().SetAlpha(INVISIBLE);
-        isInMenuSystem = false;
-    }
-    private void ShowX()
-    {
-        x.GetComponent<CanvasRenderer>().SetAlpha(VISIBLE);
-        isInMenuSystem = true;
-    }
-
-    private void ShowFloorpan()
-    {
-        floorplanVisible = true;
-        floorplan.CrossFadeAlpha(VISIBLE, CROSSFADE_TIME, false);
-    }
-
-    private void ShowInstructions()
-    {
-        instructionsVisible = true;
-        instructions.CrossFadeAlpha(VISIBLE, CROSSFADE_TIME, false);
-    }
-
-    private void HideFloorplan(bool fast = false)
-    {
-        if (fast)
-        {
-            floorplan.GetComponent<CanvasRenderer>().SetAlpha(INVISIBLE);
-        } else
-        {
-            floorplan.CrossFadeAlpha(INVISIBLE, CROSSFADE_TIME, false);    
-        }
-        floorplanVisible = false;
-    }
-
-    private void HideInstructions()
-    {
-        instructionsVisible = false;
-        instructions.CrossFadeAlpha(INVISIBLE, CROSSFADE_TIME + .2f, false);
-    }
+ 
 }
+
+
+// stripped out the animation code.
+
+// instructionsVisible = false;
+//instructions.CrossFadeAlpha(INVISIBLE, CROSSFADE_TIME, false);
+
+
+
+//if (fast)
+//{
+//    floorplan.GetComponent<CanvasRenderer>().SetAlpha(INVISIBLE);
+//} else
+//{
+//    floorplan.CrossFadeAlpha(INVISIBLE, CROSSFADE_TIME, false);    
+//}
+//floorplanVisible = false;
+
+
+//instructionsVisible = true;
+//instructions.CrossFadeAlpha(VISIBLE, CROSSFADE_TIME, false);
+
+
+//floorplan.CrossFadeAlpha(VISIBLE, CROSSFADE_TIME, false);
